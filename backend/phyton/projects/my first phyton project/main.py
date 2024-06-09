cfde9527df90e5d509db0844ffa8c45c448c4da3
tomasgasparavicius <tomas.gasparavicius12@gmail.com>
2024-06-09 12:46:16 +0300

    u
    testuojam

---
+++ backend/phyton/projects/my first phyton project/main.py
@@ -0,0 +1,18 @@
+def print_apie_mane():
+    name = "tomas"
+    spausdinimo_tekstas ="mano vardas yra "
+    metai  = 9
+    gimimo_diena= 21
+    print(spausdinimo_tekstas + name +".man "+str(metai)+" metai")
+def if_else_mokinimasis():
+    vardas = "tomas"
+    spausdinimo_tekstas = "mano vardas yra "
+#    print(spausdinimo_tekstas + vardas  )
+    a = 200
+    b = 200
+    if b > a:
+       print("b is greater than a")
+
+if __name__ == '__main__':
+    # print_apie_mane()
+    if_else_mokinimasis()
